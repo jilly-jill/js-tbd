c504817d05cf0331cfd89ebba0a961e32ddcd108
mcity42 <47469354+mcity42@users.noreply.github.com>
2022-03-24 23:58:37 -0500

    deleted old script

--- fecomponents/Fight/script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("roll").addEventListener("click", () => {
-    const randInt = Math.floor(Math.random() * 20) + 1
-    console.log(randInt)
-    document.getElementById("cube").setAttribute("class",`show-${randInt}`);
-})
