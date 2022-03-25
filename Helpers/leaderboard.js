db5f21d8149db9ef1d42fc9288b58a6707b0cb3b
mcity42 <47469354+mcity42@users.noreply.github.com>
2022-03-25 01:12:22 -0500

    made revisions and deleted lose.js

--- Helpers/leaderboard.js
+++ Helpers/leaderboard.js
@@ -1,37 +1,20 @@
 const username = require('win');
+const {userTeam , cpuTeam} = require('fight');
 
+// grab the user.name field (from /Win) and create html element for it to append
 
-
-// grab the user.name field (from /Win) and set to map
-//map the users team and the users enemies defeated
-const leaderboardMap = new Map();
 //name ,  team,  enemies
-//const username is set on /win page
-const teamsMap = new Map();
-
-const playerTeam = user.team;
-const cpuTeam = cpu.team;
-
-teamsMap.set(playerTeam, cpuTeam);
 
 
-//const name = username;
-const userTeam = teamsMap.keys();
-const enemyTeam = teamsMap.values();
+const username = username;
+userTeam= user.team;
+cpuTeam = cpu.team;
 
 
-console.log(username);
-console.log(userTeam.next().value);
-console.log(enemyTeam.next().value);
+console.log(user.team);
+console.log(userTeam);
+console.log(cpuTeam);
 
-
-leaderboardMap.set(username, teamsMap);
-
-const leaderboardKeys = leaderboardMap.keys();
-const leaderboardValues = leaderboardMap.values();
-
-console.log(leaderboardMap.keys);
-console.log(leaderboardMap.value);
 
 // Dynamically add to leaderboard 
 /* <tr>
@@ -42,30 +25,39 @@ console.log(leaderboardMap.value);
         </tr> */
         //Append to tbody
 
-const newRow = document.createElement("tr");
-const column1 = document.createElement("th");
-column1.setAttribute("scope", "row");
-column1.innerText = username;
-newRow.appendChild(column1);
+
+function createLeaderRow() {
+  const newRow = document.createElement("tr");
+  const column1 = document.createElement("th");
+  column1.setAttribute("scope", "row");
+  column1.innerText = username;
+  newRow.appendChild(column1);
+  
+  
+  const column2 = document.createElement("td");
+  column1.innerText = userTeam[0];
+  newRow.appendChild(column2);
+  
+  const column3 = document.createElement("td");
+  column1.innerText = userTeam[1];
+  newRow.appendChild(column3);
+  
+  const column4 = document.createElement("td");
+  column1.innerText = userTeam[2];
+  newRow.appendChild(column4);
+  
+  
+  const column5 = document.createElement("td");
+  column1.innerText = "Victors";
+  newRow.appendChild(column5);
+  
+  
+  const tbody = document.getElementById("tbody");
+  tbody.appendChild(newRow);
+}
 
 
-const column2 = document.createElement("td");
-column1.innerText = user.team[0];
-newRow.appendChild(column2);
 
-const column3 = document.createElement("td");
-column1.innerText = user.team[1];
-newRow.appendChild(column3);
-
-const column4 = document.createElement("td");
-column1.innerText = user.team[2];
-newRow.appendChild(column4);
-
-
-const column5 = document.createElement("td");
-column1.innerText = "Victors";
-newRow.appendChild(column5);
-
-
-const tbody = document.getElementById("tbody");
-tbody.appendChild(newRow);
+module.exports = {
+   createLeaderRow,
+}
--- Helpers/lose.js
+++
@@ -1,7 +0,0 @@
-//require('searching');
-
-
-alert(`${cpu.team} defeated ${user.team}`)
-
-
-//should we if-else for win/lose in 1 js
